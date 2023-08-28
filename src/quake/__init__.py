4fa50d1d9b9c5c770e0b2265eb810326395b2887
danangfir <danangfirmino26@gmail.com>
2023-08-28 08:26:40 +0700

    add

---
+++ src/quake/__init__.py
@@ -0,0 +1,6 @@
+def ekstrasi_data():
+    return None
+
+
+def tampilkan_data(result):
+    return None
---
+++ src/quake/example.py
@@ -0,0 +1,5 @@
+from src import quake
+
+if __name__ == '__main__':
+    result = quake.ekstrasi_data()
+    quake.tampilkan_data(result)
