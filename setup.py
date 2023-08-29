89114ca6a319880d0c76a6f5e807b1c33e73d649
danangfir <danangfirmino26@gmail.com>
2023-08-29 11:10:52 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='indoquake',
+    version='1.0.0',
+    description='A simple Python package',
+    author='danangfir',
+    author_email='danangfirmino26@gmail.com',
+    packages=['indoquake'],
+    install_requires=['beautifulsoup4', 'requests'],
+)
